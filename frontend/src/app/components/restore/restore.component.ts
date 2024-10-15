d67c0a442953fa8a25cbe19d11311c68273dd262
Emanuel Quiroga <monode3cabezas21@gmail.com>
2024-10-14 23:20:19 -0300

    Second fiumbaaaaa

--- frontend/src/app/components/restore/restore.component.ts
+++ frontend/src/app/components/restore/restore.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../home/navbar/navbar.component';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-restore',
   standalone: true,
   imports: [ NavbarComponent ],
   templateUrl: './restore.component.html',
-  styleUrl: './restore.component.css'
+  styleUrl: './restore.component.css',
+  providers: [UserService]
 })
 export class RestoreComponent {
 
--- frontend/src/app/components/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../home/navbar/navbar.component';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-shopping-cart',
   standalone: true,
   imports: [ NavbarComponent ],
   templateUrl: './shopping-cart.component.html',
-  styleUrl: './shopping-cart.component.css'
+  styleUrl: './shopping-cart.component.css',
+  providers: [UserService]
 })
 export class ShoppingCartComponent {
 
