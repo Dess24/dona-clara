d3c3a20b58c5d5177db4b2295ed742432d795b8a
Dess24 <adamianperez224@gmail.com>
2024-10-28 22:20:44 -0300

    iaiaiaiaiaia

--- frontend/src/app/components/home/hero/hero.component.ts
+++ frontend/src/app/components/home/hero/hero.component.ts
@@ -7,8 +7,36 @@ import { Component, AfterViewInit, ViewChildren, QueryList, ElementRef } from '@
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
-  
-  
+export class HeroComponent implements AfterViewInit {
+  @ViewChildren('carouselItem') carouselItems!: QueryList<ElementRef>;
+  currentIndex: number = 0;
 
+  ngAfterViewInit() {
+    this.showSlide(this.currentIndex);
+  }
+
+  showSlide(index: number, direction: 'left' | 'right' = 'left') {
+    this.carouselItems.forEach((item, i) => {
+      item.nativeElement.classList.add('hidden');
+      item.nativeElement.classList.remove('slide-in-left', 'slide-in-right', 'slide-out-left', 'slide-out-right');
+      if (i === index) {
+        item.nativeElement.classList.remove('hidden');
+        item.nativeElement.classList.add(direction === 'left' ? 'slide-in-left' : 'slide-in-right');
+      }
+    });
+  }
+
+  nextSlide() {
+    const nextIndex = (this.currentIndex + 1) % this.carouselItems.length;
+    this.carouselItems.toArray()[this.currentIndex].nativeElement.classList.add('slide-out-right');
+    this.currentIndex = nextIndex;
+    this.showSlide(this.currentIndex, 'right');
+  }
+
+  prevSlide() {
+    const prevIndex = (this.currentIndex - 1 + this.carouselItems.length) % this.carouselItems.length;
+    this.carouselItems.toArray()[this.currentIndex].nativeElement.classList.add('slide-out-left');
+    this.currentIndex = prevIndex;
+    this.showSlide(this.currentIndex, 'left');
+  }
 }
